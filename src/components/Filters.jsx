64bf2ccc51b78a97b0192473a95ebc3d5b63f5f6
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-02-17 01:13:30 +0530

    sorting

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Form, Button } from 'react-bootstrap'
+import { useState } from 'react';
+import { Rating } from './Rating';
+import { CartState } from '../context/Context';
+import { type } from '@testing-library/user-event/dist/type';
 
 export const Filters = () => {
+    const [rate, setRate] = useState(3);
+     const {productState : {byStock, byFastDelivery, sort, byRating}, productDispatch} = CartState()
+
   return (
     <div className='filters'>
         <span className='title'>Filter Products</span>
@@ -11,6 +18,11 @@ export const Filters = () => {
             name="group1"
             type="radio"
             id={`inline-1`}
+            onChange={()=>productDispatch({
+                type : "SORT_BY_PRICE",
+                payload : "lowToHigh",
+            })}
+            checked={sort=== "lowToHIgh" ? true : false}
             />
         </span>
         <span>
@@ -19,6 +31,11 @@ export const Filters = () => {
             name="group1"
             type="radio"
             id={`inline-2`}
+            onChange={()=>productDispatch({
+                type : "SORT_BY_PRICE",
+                payload : "highToLow",
+            })}
+            checked={sort=== "highToLow" ? true : false}
             />
         </span>
         <span>
@@ -26,6 +43,10 @@ export const Filters = () => {
             label="Include Out of Stock"
             name="group1"
             type="checkbox"
+            onChange={()=>productDispatch({
+                type : "FILTER_BY_STOCK",
+            })}
+            checked={byStock}
             id={`inline-3`}
             />
         </span>
@@ -35,14 +56,23 @@ export const Filters = () => {
             name="group1"
             type="checkbox"
             id={`inline-4`}
+             onChange={()=>productDispatch({
+                type : "FILTER_BY_DELIVERY",
+            })}
+            checked={byFastDelivery}
             />
         </span>
-        {/* <span>
+        <span>
             <label style={{paddingRight:10}}>Rating: </label>
-            <Rating rating={byRating} style={{cursor :"pointer" }} />
+            <Rating rating={byRating}
+            onClick={(i)=>productDispatch({
+                type : "FILTER_BY_RATING",
+                payload : i+1,
+            })} style={{cursor :"pointer" }} />
         </span>
-        */}
+       
         <Button variant="light">Clear Filters</Button> 
     </div>
   )
 }
+
